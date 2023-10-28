efbf59c5f24cd5f1323c1c018029f29ecd5ecc74
binwon-song <binwon236@gmail.com>
2023-10-28 19:51:57 +0900

    add : student class classify

--- mid_term.py
+++ mid_term.py
@@ -2,26 +2,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
+from openpyxl import load_workbook
+
 
 base_dir = "/Users/binwon/Downloads"
 file_nm = "2023-02-midterm.xlsx"
 xlsx_dir = os.path.join(base_dir, file_nm)
-
+class1_xlsx_dir = os.path.join(base_dir, "1분반-" + file_nm)
+class2_xlsx_dir = os.path.join(base_dir, "2분반-" + file_nm)
+a="/Users/binwon/Downloads/1분반-2023-02-midterm.xlsx"
+b="/Users/binwon/Downloads/2분반-2023-02-midterm.xlsx"
 result = [True, True, True, False, False, True, True, False, True, True, False, False, True, False, True]
 fault_analyze = {i: 0 for i in range(1, 16)}
 None_analyze = {i: 0 for i in range(1, 16)}
-score_dict = {}
+class1_score_dict = {}
+class2_score_dict = {}
 
 while True:
     score = 0
+    student_class=int(input("분반 입력 : "))
     student_id = input("학번 입력(입력 중단 : -1 ) : ")
     if student_id == "-1":
         break
     student_name = input("이름 입력 : ")
-    TF_input = input("TF 입력 ex) TTFFNTNF :\n")
+    TF_input = input("TF 입력 ex) TTTFFTTFTTFFTFT :\n")
     while len(TF_input) != 15:
         print("\nTF 갯수 오류! 재입력")
         TF_input = input("TF 입력 ex) TTFFNTNF :\n")
+    
 
     for q_num, q_ans in enumerate(TF_input):
 
@@ -42,8 +50,13 @@ while True:
             score -= 1
             fault_analyze[q_num + 1] += 1
         
-            
-    score_dict[student_id] = score
+    another_question=map(int,input("서술형 점수 기입 : ").split())
+    score+=sum(another_question)
+    if student_class==1:
+        class1_score_dict[student_id] = score
+    elif student_class==2:
+        class2_score_dict[student_id]=score
+        
     print(f"score : {score}")
 
 print("\n\n틀린 문제")
@@ -53,13 +66,19 @@ print("무응답 문제")
 print(None_analyze)
 print("=========================\n")
 print("점수 통계")
-print(score_dict)
+print(f"1분반 : \n{class1_score_dict}")
+print(f"2분반 : \n{class2_score_dict}")
+score_dict =dict(class1_score_dict)
+score_dict.update(class2_score_dict)
 
 # 데이터프레임 수정
-df = pd.DataFrame({"학번": list(score_dict.keys()), "TF 점수": list(score_dict.values())})
+class1_df = pd.DataFrame({"학번": list(class1_score_dict.keys()), "점수": list(class1_score_dict.values())})
+class2_df = pd.DataFrame({"학번": list(class2_score_dict.keys()), "점수": list(class2_score_dict.values())})
 
-# 엑셀 파일로 저장
-df.to_excel(xlsx_dir, sheet_name='2023, 시스템 소프트웨어 중간시험', na_rep='NaN', header=True)
+
+class1_df.to_excel(excel_writer=class1_xlsx_dir, sheet_name='2023, 1분반_시스템 소프트웨어 중간시험', na_rep='NaN', header=True)
+class2_df.to_excel(excel_writer=class2_xlsx_dir, sheet_name='2023, 2분반_시스템 소프트웨어 중간시험', na_rep='NaN', header=True)
+
 
 # 틀린 문제와 무응답 문제를 정렬하여 다시 생성
 fault_analyze = {f"Q {key}": value for key, value in sorted(fault_analyze.items(), key=lambda item: item[1], reverse=True)}
